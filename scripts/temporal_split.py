220d394f45b7842af04cead064d74b215a036de6
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2018-12-01 06:21:36 +0900

    update

--- scripts/temporal_split.py
+++ scripts/temporal_split.py
@@ -1,6 +1,8 @@
 import argparse
 import os
+import re
 
+import numpy as np
 import pandas as pd
 
 
@@ -11,9 +13,8 @@ def format_time(string: str) -> int:
 
 
 def time_format(time: int) -> str:
-    minute = time / 60
-    second = time // 60
-    return '00:{:02}:{:02}'.format(minute, second)
+    minute, second = divmod(time, 60)
+    return f'00:{minute:02}:{second:02}'
 
 
 parser = argparse.ArgumentParser()
@@ -35,17 +36,28 @@ os.makedirs(args.output_directory, exist_ok=True)
 
 csv = pd.read_csv(args.input_csv)
 
+j = 0
 for i in range(len(csv)):
     end_time = format_time(csv['end_time'][i])
     current_start_time = format_time(csv['start_time'][i])
     current_end_time = current_start_time + args.split_time
-    while current_end_time < end_time:
+    while current_end_time <= end_time:
+        if not np.isnan(csv['suffix'][i]):
+            out_name = '{}_{}_{}.mp4'.format(
+                re.sub(r'\.mp4', '', csv['video_name'][i]),
+                int(csv['suffix'][i]),
+                j
+            )
+        else:
+            out_name = '{}_{}.mp4'.format(re.sub(r'\.mp4', '', csv['video_name'][i]),
+                                          j)
         command = 'ffmpeg -y -i {} -ss {} -to {} {}'.format(
             os.path.join(args.input_directory, csv['video_name'][i]),
             time_format(current_start_time), time_format(current_end_time),
-            os.path.join(args.output_directory, csv['video_name'][i])
+            os.path.join(args.output_directory, out_name)
         )
-        print(command)
-        # os.system(command)
+        # print(command)
+        os.system(command)
         current_start_time += args.split_time
         current_end_time += args.split_time
+        j += 1
