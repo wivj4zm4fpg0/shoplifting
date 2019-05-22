6422301ab6e07394eeb2a83c2ea1538304f5929a
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2019-05-22 20:12:39 +0900

    バグ修正

--- scripts/output_cross_validation.py
+++ scripts/output_cross_validation.py
@@ -22,32 +22,38 @@ csv = pd.read_csv(args.input_file, sep=r'\s', engine='python')
 csv = csv.sort_values(by=['class', 'id'], ascending=True)
 
 divide_rate = args.divide_rate
-validation_number = int(len(csv) / divide_rate / 2)
+action_len = len([i for i in csv['class'] if i == 1])
+no_action_len = len([i for i in csv['class'] if i == 0])
+action_val_len = int(action_len / divide_rate)
+no_action_val_len = int(no_action_len / divide_rate)
 
 for i in range(divide_rate):
     with open('{}_{}.csv'.format(args.output_file, i + 1), 'w') as f:
         f.write('id video_name class subset\n')
-        validation_index_min = validation_number * i
-        validation_index_max = validation_number * (i + 1)
+        action_val_index_min = action_val_len * i
+        action_val_index_max = action_val_len * (i + 1)
+        no_action_val_index_min = no_action_val_len * i
+        no_action_val_index_max = no_action_val_len * (i + 1)
         action_count = 0
         no_action_count = 0
         subset = ''
         for j in range(len(csv)):
             if csv['class'][j] == 0:
                 no_action_count += 1
-                if validation_index_max >= no_action_count > validation_index_min:
+                if no_action_val_index_max >= no_action_count > no_action_val_index_min:
                     subset = 'validation'
                 else:
                     subset = 'training'
             else:
                 action_count += 1
-                if validation_index_max >= action_count > validation_index_min:
+                if action_val_index_max >= action_count > action_val_index_min:
                     subset = 'validation'
                 else:
                     subset = 'training'
-            f.write('{} {} {} {}\n'.format(
+            f.write('{} {}_{} {} {}\n'.format(
                 csv['id'][j],
                 re.sub(r'\.mp4', '', csv['video_name'][j]),
+                csv['id'][j],
                 csv['class'][j],
                 subset
             ))
